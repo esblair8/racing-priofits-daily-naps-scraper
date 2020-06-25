3431bb2135dc1f97c007532008b2f58c2b189e8e
Evan Blair <evan.blair@arm.com>
2020-06-25 15:59:51 +0100

    uncommented naps scraper, as still useful for research

--- src/napsScraper.js
+++ src/napsScraper.js
@@ -1,44 +1,44 @@
-// #!/usr/bin/env node
+#!/usr/bin/env node
 
-// const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer')
 
-// const $ = require('cheerio')
-// const moment = require('moment')
+const $ = require('cheerio')
+const moment = require('moment')
 
-// const write = require('./selectionFileWriter')
+const write = require('./selectionFileWriter')
 
-// const url = 'https://www.racingpost.com/tipping/naps-table'
-// const todaysDate = moment().format('YYYY-MM-DD')
+const url = 'https://www.racingpost.com/tipping/naps-table'
+const todaysDate = moment().format('YYYY-MM-DD')
 
-// console.log('Analysing Naps Table.')
+console.log('Analysing Naps Table.')
 
-// puppeteer.launch()
-//     .then(browser => {
-//         console.info('Starting set up.')
-//         return browser.newPage()
-//     })
-//     .then(page => {
-//         const content = page.goto(url).then(() => page.content())
-//         console.info('Set up complete.')
-//         return content
-//     })
-//     .then(html => {
-//         console.info('Parsing html.')
-//         return $('.tn-napsTable__row', html).map(function () {
-//             return {
-//                 date: todaysDate,
-//                 time: $('.tn-napsTable__time', this).text(),
-//                 course: $('.tn-napsTable__crs', this).text(),
-//                 selection: $('.tn-napsTable__naps', this).text().replace('right', '').trim()
-//             }
-//         })
-//     })
-//     .then(selections => {
-//         return write(selections.toArray(), 'naps.csv')
-//     })
+puppeteer.launch()
+    .then(browser => {
+        console.info('Starting set up.')
+        return browser.newPage()
+    })
+    .then(page => {
+        const content = page.goto(url).then(() => page.content())
+        console.info('Set up complete.')
+        return content
+    })
+    .then(html => {
+        console.info('Parsing html.')
+        return $('.tn-napsTable__row', html).map(function () {
+            return {
+                date: todaysDate,
+                time: $('.tn-napsTable__time', this).text(),
+                course: $('.tn-napsTable__crs', this).text(),
+                selection: $('.tn-napsTable__naps', this).text().replace('right', '').trim()
+            }
+        })
+    })
+    .then(selections => {
+        return write(selections.toArray(), 'naps.csv')
+    })
 
-//     .then(documentPath => {
-//         console.info(`Naps for ${todaysDate} written to ${documentPath}`)
-//         process.exit()
-//     })
-//     .catch(err => console.log('Error', err))
+    .then(documentPath => {
+        console.info(`Naps for ${todaysDate} written to ${documentPath}`)
+        process.exit()
+    })
+    .catch(err => console.log('Error', err))
